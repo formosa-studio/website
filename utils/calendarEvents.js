7cafd1a83b96c18bd43bfca6916e886b9d531ff2
diamord_tw <84235575+diamord@users.noreply.github.com>
2025-03-28 22:59:49 +0800

    280325 Progress - Event API

--- utils/calendarEvents.js
+++ utils/calendarEvents.js
@@ -1,57 +1,24 @@
 module.exports = async () => {
-    /*const axios = require("axios");
+    const axios = require("axios");
     const fs = require("fs");
     const path = require("path");
 
-    const token = JSON.parse(fs.readFileSync(path.join(__dirname, "zohoToken.json"))).access_token;
-    const url = `https://calendar.zoho.com/api/v1/calendars/${process.env.ZOHO_CALENDAR_ID}/events?scope=ZohoCalendar.event.READ`;
+    const token = process.env.TU_Token
+    const url = `https://api.teamup.com/kscgrtk5zmwe8cndts/events`;
 
     try {
         const response = await axios.get(url, {
             headers: {
-                Authorization: `Bearer ${token}`
+                Authorization: `Teamup-Token ${token}`
             }
         });
 
-        return response.data;
+        return response.data.events;
     } catch (error) {
         console.error("calendarEvents: 無法獲取活動資訊: ", error.message);
         return null;
     }
-
-    const calendarEvents = [
-        {
-            "title": "駕駛員培訓 | SkibidiBallPlayer",
-            "dateandtime": {
-                "start": "20250323T135000+0531",
-            },
-        },
-        {
-            "title": "站務員培訓 | SigmaGamer",
-            "dateandtime": {
-                "start": "20250323T205000+0530",
-            },
-        },
-        {
-            "title": "駕駛員培訓 | BestKid",
-            "dateandtime": {
-                "start": "20250323T205000+0530",
-            },
-        },
-        {
-            "title": "列車長培訓 | WorstKid",
-            "dateandtime": {
-                "start": "20250323T205000+0530",
-            },
-        },
-        {
-            "title": "調度員培訓 | SigmaGamer",
-            "dateandtime": {
-                "start": "20250323T205000+0530",
-            },
-        },
-    ]
-
+    
     const formattedEvents = calendarEvents.map(event => {
         const [trainingType, playerName] = event.title.split(" | ");
         const rawDate = event.dateandtime.start;
@@ -75,7 +42,4 @@ module.exports = async () => {
     });
 
     return formattedEvents;
-}*/
-
-return null;
 }
